594c4207cf4298da728423b509168c5ca43d3dd5
Andrew <andrewgardner@live.com>
2018-11-30 13:08:43 -0700

    pt 4 complete no constraints yet

--- DataApp/Controller/BucketItemCell.swift
+++ DataApp/Controller/BucketItemCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BucketItemCellTable: UITableViewCell {
+class BucketItemCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,6 +19,52 @@ class BucketItemCellTable: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    @IBOutlet weak var bucketItemSymbol: UILabel!
+    
+    @IBOutlet weak var bucketItemText: UILabel!
+    
+    @IBOutlet weak var bucketItemSignature: UILabel!
+
+    var currentBucketItem : BucketItem!
+    {
+        didSet
+        {
+            updateCellView()
+        }
+    }
+    
+    private func randomEmoji()->String
+    {
+        let emojiStart = 0x1f601
+        let emojiEnd = 0x1F64F
+        let symbolStart = 0x1F680
+        let symbolEnd = 0x1F6C5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart +
+            Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
+    }
+    
+    private func updateCellView() -> Void
+    {
+        if (currentBucketItem != nil)
+        {
+            bucketItemSignature.text = currentBucketItem.itemAuthor
+            bucketItemText.text = currentBucketItem.itemContents
+        }
+        else
+        {
+            bucketItemSignature.text = "author goes here"
+            bucketItemText.text = "bucket item contents here"
+        }
+        
+        bucketItemSymbol.text = randomEmoji()
     }
 
 }
